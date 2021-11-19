05f19511d20366dd7d8679aa192ec0e790b54827
steveoni <steohenoni2@gmail.com>
2021-11-19 14:47:20 +0100

    [dp_babbage][s]: copy babbagemodel from datapackage.json

---
+++ datapackage_pipelines_fiscal/processors/dp_babbage.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+import logging
+import json
+
+from datapackage_pipelines.wrapper import process
+
+def copy_babbage(dp, parameters, *_):
+    
+    abs_path = os.path.abspath("normalized")
+    logging.info(f'Obtaining Babbage model from: {abs_path}')
+
+    dpkg_path  = f'{abs_path}/final/datapackage.json'
+    dpkg = json.load(open(dpkg_path,"r"))['babbageModel']
+
+    org_path = os.path.dirname(os.getcwd())
+    model_name = os.path.basename(os.getcwd())
+
+    model_path = f'{org_path}/models/{model_name}.json'
+
+    with open(model_path, "w") as outfile:
+        json.dump(dpkg, outfile)
+
+    return dp
+
+if __name__ == '__main__':
+    process(modify_datapackage=copy_babbage)
